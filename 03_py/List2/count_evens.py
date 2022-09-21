5b34ef2913858efb2e527e405766d9c31ac77388
Brian <brianyang1206@gmail.com>
2022-09-21 19:30:24 -0400

    List 2 test cases

--- 03_py/List2/count_evens.py
+++ 03_py/List2/count_evens.py
@@ -5,3 +5,12 @@ def count_evens(nums):
     if i % 2 == 0:
       a+=1
   return a
+
+print('count_evens([2, 1, 2, 3, 4]):',count_evens([2, 1, 2, 3, 4]))
+print('count_evens([2, 2, 0]):',count_evens([2, 2, 0]))
+print('count_evens([1, 3, 5]):',count_evens([1, 3, 5]))
+print('count_evens([]):',count_evens([]))
+print('count_evens([11, 9, 0, 1]):',count_evens([11, 9, 0, 1]))
+print('count_evens([2, 11, 9, 0]):',count_evens([2, 11, 9, 0]))
+print('count_evens([2]):',count_evens([2]))
+print('count_evens([2, 5, 12]):',count_evens([2, 5, 12]))
