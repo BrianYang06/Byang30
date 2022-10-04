5b8a40793c77ef9f86c75db8d1bcb3b0cc5c1939
Brian Yang <brianyang1206@gmail.com>
2022-10-04 12:02:51 -0400

    Updated some answers

--- 07-teardown/app.py
+++ 07-teardown/app.py
@@ -8,9 +8,9 @@ time spent: .30 hrs
 
 from flask import Flask
 
-app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs? Python __main__
+app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs? Python __main__ or java constructors
 
-@app.route("/") # Q1: What points of reference do you have for meaning of '/'? File location
+@app.route("/") # Q1: What points of reference do you have for meaning of '/'? File location/Folders are seperated by / in file explorer
 def hello_world():
     print(__name__) # Q2: Where will this print to? Q3: What will it print? Probably the console. It will print the name of the app
     return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?  No bc the return is never used
