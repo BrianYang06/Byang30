7e445ea32716164c0fbfd4fc54fa91bb89883ef3
Brian <brianyang1206@gmail.com>
2022-10-12 20:11:58 -0400

    added heading and everything

--- 09_flask-static/app.py
+++ 09_flask-static/app.py
@@ -1,14 +1,32 @@
+"""
+Brian Yang, Donald Bi, Faiyaz Rafee - Dual Ducks
+SoftDev
+K09 -- Using flask to serve static files.
+2022-10-12
+time spent: .5 hours
+
+Disco:
+- Static webpages never change
+- If you dont have a file extension and you try to open it on the webpage it downloads the files
+- Static webpages are good for showing Dq1
+
+QCC:
+- What if you put non .html files in there and try to open it on the webpage?
+- What exactly makes flask recognize the static folder and allow for it to work?
+"""
+
+
 # Clyde 'Thluffy' Sinclair
 # SoftDev
 # Oct 2022
 
-# DEMO 
+# DEMO
 # basics of /static folder
 
 from flask import Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route("/")       
+@app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
