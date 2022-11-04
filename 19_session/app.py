26b7001a72fbe5d3ee652b84720f4505b8531fb3
Brian Yang <brianyang1206@gmail.com>
2022-11-04 12:19:47 -0400

    Copying partner Moohabir's code

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,39 +1,50 @@
+# Verit Li, Brian Yang, Justin Mohabir
+# Team: Uh?
+# SoftDev
+# K19: Sessions Greetings
+# 2022-10-17
+# time spent: .6
+
 from flask import Flask, render_template, session, request, redirect, url_for
+import os
 app = Flask(__name__)
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = os.urandom(32)
 
 from flask import session
 
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+the_username = "verit"
+the_password = "getstitches"
 
-username = "Verit"
-password = "getstitches"
-@app.route('/')
-def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
-
-@app.route('/login', methods=['GET', 'POST'])
+#This the login page
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-                <br> <input type = text name=password>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+    if 'username' in session:
+        if session['username'] == the_username and session['password'] == the_password:
+            return render_template("response.html", user=session['username'])
+        else:
+            # Must highlight what the user did wrong
+            wrongdoings=""
+            if session['username'] != the_username:
+                wrongdoings+=' Username Wrong'
+            if session['password'] != the_password:
+                wrongdoings+=' Password Wrong'
+            return render_template("error.html",huhs=wrongdoings)
+    return render_template("login.html")
+
+#auth is called after a form is submitted as seen in the html file
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+    return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.debug = True
