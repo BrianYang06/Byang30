0386afe6e6628f4fb5862080d0d9a3d7bf99ad8f
Brian Yang <brianyang1206@gmail.com>
2022-11-22 13:37:02 -0500

    touchups

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,5 +1,5 @@
 """
-Brian Yang, Jian Hong li
+Kung Pao Chicken: Brian Yang, Jian Hong li
 SoftDev
 k20 -- RESTful API
 2022 -- 11 -- 21
@@ -12,14 +12,14 @@ import requests
 app = Flask(__name__)    #create Flask object
 
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    try:
+    try: #check for if key_nasa.txt exists
         with open("key_nasa.txt", "r") as file:
             api_key = file.read()
-            request_data = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key)
+            request_data = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key) #returns a requests.models.Response class that need to be made into a json
             data = request_data.json()
-            image = data["hdurl"]
+            image = data["hdurl"] #Select the thing we want from the json
             return render_template('main.html', img = image)
     except:
         return render_template('main.html', error = "No/Wrong API key provided")
